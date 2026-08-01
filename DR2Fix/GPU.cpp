5f5807949135afe81f147e9ecb3694d5d7acaf17
Clownacy <Clownacy@users.noreply.github.com>
2026-08-01 18:17:37 +0100

    Make shadow/mirror resolution scale with display resolution.
    Fixes dreadful performance on the Steam Deck.

--- DR2Fix/GPU.cpp
+++ DR2Fix/GPU.cpp
@@ -3,11 +3,6 @@
 #include "General.h"
 #include "Utils.h"
 
-static constexpr int ResMultiplier        =    4;
-static constexpr int ShadowMapRes         = 1024 * ResMultiplier;
-static constexpr int MirrorReflectionResX = 1024 * ResMultiplier;
-static constexpr int MirrorReflectionResY =  256 * ResMultiplier;
-
 static IDirect3DDevice9* pDevice;
 static int* DrawInst;
 static short D3DDeviceOffset = 0x198;
@@ -18,6 +13,34 @@ static void (*InitShaders)();
 int* GPU::GraphicsInst;
 int GPU::ResX;
 int GPU::ResY;
+
+static unsigned int ScaleResolution(unsigned int resolution)
+{
+    const int screen_width = *(int*)(*GPU::GraphicsInst + 0xC);
+
+    // We scale based on the resolution so that we do not overwhelm low-end platforms with massive textures.
+    // 1280x720:  1x
+    // 1920x1080: 2x
+    // 2560x1440: 4x
+    // There is no 3x because we need to maintain powers of two.
+    if (screen_width >= 2560)
+        resolution <<= 2;
+    else if (screen_width >= 1920)
+        resolution <<= 1;
+
+    return resolution;
+}
+
+static unsigned int CorrectTextureFormat(unsigned int format)
+{
+    const int screen_width = *(int*)(*GPU::GraphicsInst + 0xC);
+
+    if (screen_width >= 1920)
+        if (format == 3) // RGB565
+            format = 1;  // XRGB8888
+
+    return format;
+}
 
 static void GetGPUVendor() {
     if (General::IsOTR) D3DDeviceOffset += 0x100;
@@ -55,6 +78,7 @@ static void FixRenderStates(safetyhook::Context32& ctx) {
     if (pRenderTarget) {
         D3DSURFACE_DESC Desc;
         pRenderTarget->GetDesc(&Desc);
+	const unsigned int ShadowMapRes = ScaleResolution(1024);
         /*unsigned int ShaderHash = *(unsigned int*)(*DrawInst + Shader::ShaderHashOffset);
         if (Shader::IsSkinnedShader(ShaderHash) && Desc.Width == ResX && Desc.Height == ResY) {
             pDevice->SetRenderState(D3DRS_CULLMODE, D3DCULL_CW);
@@ -63,6 +87,20 @@ static void FixRenderStates(safetyhook::Context32& ctx) {
         else*/ if (Desc.Width == ShadowMapRes * 4 && Desc.Height == ShadowMapRes) pDevice->SetRenderState(D3DRS_ALPHABLENDENABLE, FALSE);
         pRenderTarget->Release();
     }
+}
+
+static int (__fastcall *CreateTexture)(void *self, void* dummy, int width, int height, int format, int a5, int a6, int multisample_type, const char *label);
+
+static int __fastcall CreateTextureHijack(void *self, void* dummy, int width, int height, int format, int a5, int a6, int multisample_type, const char *label)
+{
+    return CreateTexture(self, dummy, ScaleResolution(width), ScaleResolution(height), CorrectTextureFormat(format), a5, a6, multisample_type, label);
+}
+
+static int (__fastcall *CreateTextureRenderTarget)(void *self, void* dummy, int width, int height, int format, int a5, unsigned int a6, int multisample_type);
+
+static int __fastcall CreateTextureRenderTargetHijack(void *self, void* dummy, int width, int height, int format, int a5, unsigned int a6, int multisample_type)
+{
+    return CreateTextureRenderTarget(self, dummy, ScaleResolution(width), ScaleResolution(height), CorrectTextureFormat(format), a5, a6, multisample_type);
 }
 
 void GPU::Install() {
@@ -77,19 +115,13 @@ void GPU::Install() {
     DrawInst = *(int**)(Pattern.get_first(0x06));
 
     // Increase resolution of shadow map.
-    Pattern = Utils::FindPattern("83 BE ? ? ? ? ? C7 86 ? ? ? ? ? ? ? ? C6 86");
-    static auto ShadowRes = safetyhook::create_mid(Pattern.get_first(0x11), [](SafetyHookContext& ctx) {
-        const short WidthOffset = General::IsOTR ? 0xD0 : 0xBC;
-        *(int*)(ctx.esi + WidthOffset) = ShadowMapRes;
-       });
+    Pattern = Utils::FindPattern("6A 00 83 C0 01 6A 19 53 89 0C 85");
+    InterceptCall(Pattern.get_first(0x22), CreateTexture, &CreateTextureHijack);
+    InterceptCall(Pattern.get_first(0x50), CreateTextureRenderTarget, &CreateTextureRenderTargetHijack);
 
-    // Increase resolution of mirror reflection texture.
+    // Increase resolution of mirror reflection textures.
+    // Also changes format from RGB565 to XRGB8888, to eliminate colour-banding.
     Pattern = Utils::FindPattern("6A 19 C7 86 ? ? ? ? 00 00 00 00 8B 3D ? ? ? ? 68 00 01 00 00 68 00 04 00 00 8B CF E8");
-    Patch(Pattern.get_first(0x13), MirrorReflectionResY);
-    Patch(Pattern.get_first(0x18), MirrorReflectionResX);
-    Patch(Pattern.get_first(0x4B), MirrorReflectionResY);
-    Patch(Pattern.get_first(0x64), MirrorReflectionResX);
-
-    // Change mirror reflection format from RGB565 to XRGB8888, to eliminate colour-banding.
-    Patch(Pattern.get_first(0x40), {1});
+    InjectHook(Pattern.get_first(0x1E), &CreateTextureHijack);
+    InjectHook(Pattern.get_first(0x70), &CreateTextureRenderTargetHijack);
 }
